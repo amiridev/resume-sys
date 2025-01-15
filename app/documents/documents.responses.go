1e8978bcdd7081b7ec5897081682db60435ee835
Marzieh Amiri <m.amiri.dev@gmail.com>
2025-01-15 14:12:09 +0330

    document responses

---
+++ app/documents/documents.responses.go
@@ -0,0 +1,66 @@
+package documents
+
+import (
+	"resume-sys/database/models"
+	"time"
+)
+
+type ResponseType map[string]any
+
+type Document struct {
+	ID              string    `json:"id"`
+	UserID          string    `json:"user_id"`
+	Title           string    `json:"title"`
+	Category        string    `json:"Category"`
+	Ratings         int       `json:"Ratings"`
+	Description     string    `json:"Description"`
+	Status          string    `json:"status"`
+	PublicationDate time.Time `json:"PublicationDate"`
+}
+
+// one document response
+
+func DocumentTransform(document models.Document) Document {
+	return Document{
+		ID:              document.ID,
+		UserID:          document.UserID,
+		Title:           document.Title,
+		Category:        document.Category,
+		Ratings:         document.Ratings,
+		Description:     document.Description,
+		Status:          document.Status,
+		PublicationDate: document.PublicationDate,
+	}
+}
+
+type DocumentResponseType struct {
+	Documents Document `json:"Document"`
+}
+
+func DocumentResponse(document models.Document) ResponseType {
+	return ResponseType{
+		"document": DocumentTransform(document),
+	}
+}
+
+// list of document response
+
+func DocumentsTransform(documents []models.Document) []Document {
+	var data = []Document{}
+
+	for _, document := range documents {
+		data = append(data, DocumentTransform(document))
+	}
+
+	return data
+}
+
+type DocumentsResponseType struct {
+	Documents []Document `json:"documents"`
+}
+
+func DocumentsResponse(documents []models.Document) ResponseType {
+	return ResponseType{
+		"documents": DocumentsTransform(documents),
+	}
+}
