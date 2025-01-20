9fde0e58c2e94dea502d308e78cde112d6d3ecb3
Mostafa Gholami <mostafagholamidev@gmail.com>
2025-01-20 20:40:27 +0330

    skill module fixed
    Signed-off-by: Mostafa Gholami <mostafagholamidev@gmail.com>

--- app/skills/skills.dto.go
+++ app/skills/skills.dto.go
@@ -2,14 +2,14 @@ package skills
 
 type SkillCreateDto struct {
 	Title       string `json:"title" binding:"required,min=2,max=250"`
-	Level       string `json:"level" binding:"required, min=2 , max=250"`
+	Level       string `json:"level" binding:"required,min=2,max=250"`
 	ImageUrl    string `json:"image_url" binding:"required,min=10,max=250"`
 	Description string `json:"description" binding:"required"`
 }
 
 type SkillUpdateDto struct {
 	Title       string `json:"title" binding:"required,min=2,max=250"`
-	Level       string `json:"level" binding:"required, min=2 , max=250"`
+	Level       string `json:"level" binding:"required,min=2,max=250"`
 	ImageUrl    string `json:"image_url" binding:"required,min=10,max=250"`
 	Description string `json:"description" binding:"required"`
 }
